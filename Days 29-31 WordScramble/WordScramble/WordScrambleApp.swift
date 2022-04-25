f21ebf30a9824bce5b2a54b29a3c6dee1963603c
Lucashongcai <30290962+Lucashongcai@users.noreply.github.com>
2022-04-25 15:32:53 +0200

    Add files via upload

---
+++ Days 29-31 WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Lucas Hong Cai on 22/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
